9321aefbd161333056c07729487d3f96d312a338
Булаев Алексей <aracool1982@gmail.com>
2020-03-26 20:18:51 +0700

    HPBar

---
+++ Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] Gradient _gradient;
+    [SerializeField] Image _fill;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+        _gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int value)
+    {
+        _slider.value += value;
+        _fill.color = _gradient.Evaluate(_slider.normalizedValue);
+    }
+}
